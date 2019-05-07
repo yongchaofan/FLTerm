9b66258385a9f9c052e271b1b7960799998efdcd
yongchaofan <30712281+yongchaofan@users.noreply.github.com>
2019-05-07 18:44:58 -0500

    Release 1.0 visual studio build command

---
+++ make.cmd
@@ -0,0 +1,5 @@
+rc res\tinyTerm2.rc
+
+cl /c -Ox /MD /DWIN32 /I../%Platform%/include src/tinyTerm2.cxx src/Fl_Term.cxx src/host.cxx src/ssh2.cxx src/Fl_Browser_Input.cxx 
+
+link /SUBSYSTEM:WINDOWS tinyTerm2.obj Fl_Term.obj Fl_Browser_Input.obj host.obj ssh2.obj res\tinyTerm2.res user32.lib gdi32.lib comdlg32.lib comctl32.lib ole32.lib shell32.lib ws2_32.lib uuid.lib bcrypt.lib crypt32.lib shlwapi.lib Advapi32.lib ../%Platform%/lib/libssh2.lib ../%Platform%/lib/fltk.lib /out:tinyTerm2_%Platform%.exe
--- vs_cl.cmd
+++
@@ -1,5 +0,0 @@
-rc res\flTerm.rc
-
-cl /c -Ox /MD /DWIN32 /I../%Platform%/include src/flTerm.cxx src/Fl_Term.cxx src/Hosts.cxx src/ssh2.cxx src/Fl_Browser_Input.cxx 
-
-link /SUBSYSTEM:WINDOWS flTerm.obj Fl_Term.obj Fl_Browser_Input.obj Hosts.obj ssh2.obj res\flTerm.res user32.lib gdi32.lib comdlg32.lib comctl32.lib ole32.lib shell32.lib ws2_32.lib uuid.lib bcrypt.lib crypt32.lib shlwapi.lib Advapi32.lib ../%Platform%/lib/libssh2.lib ../%Platform%/lib/fltk.lib /out:flTerm_%Platform%.exe
