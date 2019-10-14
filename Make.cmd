e26f8b362dfdf8ed659bfa987e165c3d6e0581a7
yongchaofan <30712281+yongchaofan@users.noreply.github.com>
2019-10-13 21:16:22 -0500

    Release 1.1.1, compile with  /NXCOMPAT /DYNAMICBASE

--- Make.cmd
+++ Make.cmd
@@ -2,4 +2,4 @@ rc res\tiny2.rc
 
 cl /c -O1 /GL /MT /DWIN32 /I../%Platform%/include src/tiny2.cxx src/Fl_Term.cxx src/host.cxx src/ssh2.cxx src/Fl_Browser_Input.cxx 
 
-link /LTCG /NODEFAULTLIB:libucrt.lib ucrt.lib /SUBSYSTEM:WINDOWS tiny2.obj Fl_Term.obj Fl_Browser_Input.obj host.obj ssh2.obj res\tinyTerm2.res user32.lib gdi32.lib comdlg32.lib comctl32.lib ole32.lib shell32.lib ws2_32.lib uuid.lib bcrypt.lib crypt32.lib shlwapi.lib Advapi32.lib ../%Platform%/lib/libssh2.lib ../%Platform%/lib/fltk.lib /out:tinyTerm2_%Platform%.exe
+link /LTCG /NXCOMPAT /DYNAMICBASE /NODEFAULTLIB:libucrt.lib ucrt.lib /SUBSYSTEM:WINDOWS tiny2.obj Fl_Term.obj Fl_Browser_Input.obj host.obj ssh2.obj res\tinyTerm2.res user32.lib gdi32.lib comdlg32.lib comctl32.lib ole32.lib shell32.lib ws2_32.lib uuid.lib bcrypt.lib crypt32.lib shlwapi.lib Advapi32.lib ../%Platform%/lib/libssh2.lib ../%Platform%/lib/fltk.lib /out:tinyTerm2_%Platform%.exe
